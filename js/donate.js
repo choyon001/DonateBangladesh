4f5a55186fc85d4e3180e50e45f9900bde2af0d7
choyon001 <dewanmonjur005@gmail.com>
2025-02-11 17:01:21 +0600

    modal and donate secton functionality added

--- js/donate.js
+++ js/donate.js
@@ -1,29 +1,41 @@
-document.getElementById("donate1_submit").addEventListener("click", function(event) {
-    event.preventDefault();
-    const donate1_input = document.getElementById("donate1_input").value;
-    if (isNaN(donate1_input)) {
-        alert("Please enter a valid number.");
-    }
-    else if(donate1_input == "") {
-        alert("Please enter a number.");
-    }
-    else if(donate1_input < 0) {
-        alert("Please enter a positive number.");
-    }
-    else {
-        let donate1_current_bal = document.getElementById("donate1_current_bal").innerText;
-
-        let donate1_input = document.getElementById("donate1_input").value;
-
-        let main_bal = document.getElementById("main_bal").innerText;
-        const donate1_total = parseFloat(donate1_current_bal) + parseFloat(donate1_input);
-        document.getElementById("donate1_current_bal").innerText = donate1_total;
-
-        const main_total = parseFloat(main_bal) - parseFloat(donate1_input);
-        document.getElementById("main_bal").innerText = main_total;
-
+function donate(donateSubmit, donateInput, donateCurrentBal, mainBal) {
+    document.getElementById(donateSubmit).addEventListener("click", function(event) {
+        event.preventDefault();
         
+        const donateInputValue = document.getElementById(donateInput).value;
+        if (isNaN(donateInputValue) || donateInputValue === "") {
+            alert("Please enter a valid number.");
+            return;
+        }
+        
+        const donateAmount = parseFloat(donateInputValue);
+        if (donateAmount < 0) {
+            alert("Please enter a positive number.");
+            return;
+        }
+        
+        let donateCurrentBalValue = parseFloat(document.getElementById(donateCurrentBal).innerText);
+        let mainBalValue = parseFloat(document.getElementById(mainBal).innerText);
+        
+        const newDonateTotal = donateCurrentBalValue + donateAmount;
+        const newMainTotal = mainBalValue - donateAmount;
+        
+        document.getElementById(donateCurrentBal).innerText = newDonateTotal;
+        document.getElementById(mainBal).innerText = newMainTotal;
+        
+        console.log("Donation amount: " + donateAmount);
 
-        console.log("Donation amount: " + donate1_current_bal);
-    }
+        document.getElementById("my_modal_1").showModal();
+    });
+}
+
+
+donate("donate1_submit", "donate1_input", "donate1_current_bal", "main_bal");
+donate("donate2_submit", "donate2_input", "donate2_current_bal", "main_bal");
+donate("donate3_submit", "donate3_input", "donate3_current_bal", "main_bal");
+
+// modal close
+
+document.getElementById("my_modal_1").addEventListener("click", function() {
+    document.getElementById("my_modal_1").close();
 });
